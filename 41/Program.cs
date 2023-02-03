f049d07f35e40296f31f7cea050cd90bf3f63196
eugene <ojgenvi@gmail.com>
2023-02-03 22:16:29 +0300

    D/z

---
+++ 41/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3 */
+
+Console.WriteLine("введите M чисел через пробел");
+string[] digits = GetDigits();
+Console.WriteLine($"Чисел больше 0 - {FindPositiveDigits(digits)}");
+
+string[] GetDigits()
+{
+    string? text = Console.ReadLine();
+    return text.Split(' ');
+}
+
+int FindPositiveDigits(string[] digits)
+{
+    int positiveDigits = 0;
+    foreach (string digit in digits)
+    {
+        int num = Convert.ToInt32(digit);
+        if (num > 0)
+        {
+            positiveDigits += 1;
+        }
+    }
+    return positiveDigits;
+}
---
+++ 43/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+double k1 = GetNum("k1");
+double k2 = GetNum("k2");
+double b1 = GetNum("b1");
+double b2 = GetNum("b2");
+Console.WriteLine($"({getX(b1, b2, k1, k2)};{getY(b1, b2, k1, k2)})");
+
+double GetNum(string Value)
+{
+    Console.Write($"Введите {Value} = ");
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double getX(double b1, double b2, double k1, double k2)
+{
+    return (b2 - b1) / (k1 - k2);
+} 
+
+double getY(double b1, double b2, double k1, double k2)
+{
+    return k1 * (b2 - b1) / (k1 - k2) + b1;
+}
